360ad1d489f5289909c318695647ce879330eef5
David Li <david.li@kit.edu>
2025-04-03 14:26:20 +0200

    copying output to shared ws and give rights

--- submission_scripts/submit_horeka.sh
+++ submission_scripts/submit_horeka.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=test
+#SBATCH --job-name=4g32b3e
 #SBATCH --partition=accelerated
-#SBATCH --time=00:10:00
+#SBATCH --time=00:05:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
 #SBATCH --account=hk-project-p0021348
-#SBATCH --output="/hkfs/work/workspace/scratch/vm6493-resnet/ResNet/experiments/test/%j/slurm_%j"
+#SBATCH --output="/hkfs/work/workspace/scratch/vm6493-resnet_imagenet/ResNet/experiments/4g32b3e/%j/slurm_%j"
 #SBATCH --exclusive
 
 module purge
@@ -20,22 +20,22 @@ master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 echo "MASTER_ADDR="$MASTER_ADDR
 
-source /hkfs/work/workspace/scratch/vm6493-resnet/venv/bin/activate
+source /hkfs/work/workspace/scratch/vm6493-resnet_imagenet/venv/bin/activate
 
 if [ -n "$SLURM_NPROCS" ]; then
     export NUM_GPUS=$SLURM_NPROCS
 else
     export NUM_GPUS=0    
 fi
-export LOCAL_BATCHSIZE=256
+export LOCAL_BATCHSIZE=32
 export BATCHSIZE=$(($LOCAL_BATCHSIZE * $NUM_GPUS))
 export NUM_EPOCHS=10
-export NUM_WORKERS=8
+export NUM_WORKERS=4
 export RANDOM_SEED=0
 
-export PYDIR=/hkfs/work/workspace/scratch/vm6493-resnet/ResNet
+export PYDIR=/hkfs/work/workspace/scratch/vm6493-resnet_imagenet/ResNet
 export EXP_BASE=${PYDIR}/experiments
-export EXP_TYPE=${EXP_BASE}/${NUM_GPUS}g${BATCHSIZE}b${NUM_EPOCHS}e
+export EXP_TYPE=${EXP_BASE}/${NUM_GPUS}g${LOCAL_BATCHSIZE}b${NUM_EPOCHS}e
 mkdir ${EXP_TYPE}
 export RESDIR=${EXP_TYPE}/${SLURM_JOB_ID}
 echo $RESDIR
@@ -49,7 +49,7 @@ cd ${RESDIR}
 
 # arguments for the training:
 # --use_subset: optional. if used, then only small amount of data set is used in training for faster debugging
-# --data_path: path to training, valid and test data
+# --data_path: path to training, valid and 4g32b3e data
 # --batchsize: global batch size
 # --num_epochs: number of epochs the model will be trained
 # --seed: to enable deterministic training
@@ -58,3 +58,10 @@ srun -u --mpi=pmi2 bash -c "
         PERUN_APP_NAME=$PERUN_APP_NAME \
         perun monitor --data_out=$PERUN_OUT --app_name=$PERUN_APP_NAME ${PYDIR}/scripts/main.py \
         --data_path ${DATA_PATH} --batchsize ${BATCHSIZE} --num_epochs ${NUM_EPOCHS} --num_workers ${NUM_WORKERS} --seed ${RANDOM_SEED} --use_subset"
+
+export SHARED_WS=/hkfs/work/workspace/scratch/vm6493-resnet/experiments/${NUM_GPUS}g${LOCAL_BATCHSIZE}b${NUM_EPOCHS}e
+# Create the shared workspace
+mkdir -p ${SHARED_WS}
+# Copy the results to the shared workspace
+cp -r ${RESDIR} ${SHARED_WS}
+setfacl -Rm u:xy6660:rwX,d:u:xy6660:rwX ${SHARED_WS}
