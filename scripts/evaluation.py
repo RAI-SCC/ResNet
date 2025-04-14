3dfa8c0309fcb4b8d4651d097a93f8ef11fa6ea4
David Li <david.li@kit.edu>
2025-04-14 15:49:36 +0200

    added slurm id list to specify plots with same hyperparams

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -5,16 +5,33 @@ from resnet.eval_utils.plot_utils import plot_scaling, plot_top1
 
 
 # result_path = Path("/home/scc/xy6660/ResNet/ResNet/experiments/")
-result_path = Path("/Users/philipphuber/Documents/Projects/ResNet/ResNet/experiments/")
+result_path = Path("/hkfs/work/workspace/scratch/vm6493-resnet_imagenet/ResNet/experiments")
 
-strong_scaling_list = ["32g256b4w100e", "64g128b4w100e", "128g64b4w100e", "256g32b4w100e"]
-weak_scaling_list = ["16g256b4w100e", "32g256b4w100e", "64g256b4w100e", "128g256b4w100e", "256g256b4w100e"]
+constant_gbs_list = ["32g256b4w100e", "64g128b4w100e", "128g64b4w100e", "256g32b4w100e"]
+constant_gbs_slurms = [3040217, 3047906, 3047836, 3047855]
+constant_gbs_path =[]
+for i in range(len(constant_gbs_list)):
+    constant_gbs_path.append(
+        Path(
+            constant_gbs_list[i], str(constant_gbs_slurms[i])
+            )
+        )
 
 
-plot_scaling(result_path=result_path, scaling_list=strong_scaling_list, name="resnet8192gbs")
-plot_scaling(result_path=result_path, scaling_list=weak_scaling_list, name="resnet256lbs")
+constant_lbs_list = ["32g256b4w100e", "64g256b4w100e", "128g256b4w100e", "256g256b4w100e"]
+constant_lbs_slurms = [3040217, 3040210, 3040222, 3062064]
+constant_lbs_path = []
+for i in range(len(constant_lbs_list)):
+    constant_lbs_path.append(
+        Path(
+            constant_lbs_list[i], str(constant_lbs_slurms[i])
+            ))
 
-plot_top1(result_path, strong_scaling_list, name="resnet_sslist", key="ss")
-plot_top1(result_path, weak_scaling_list, name="resnet_wslist", key="ws")
+
+plot_scaling(result_path=result_path, scaling_list=constant_gbs_path, name="resnet8192gbs")
+plot_scaling(result_path=result_path, scaling_list=constant_lbs_path, name="resnet256lbs")
+
+plot_top1(result_path, constant_gbs_path, name="resnet_lbslist", key="ss")
+plot_top1(result_path, constant_lbs_path, name="resnet_gbslist", key="ws")
 
 # h5val.visititems(print_attrs)
